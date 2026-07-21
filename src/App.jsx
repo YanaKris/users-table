161764572c1d54ccaab8ad765429776dd6cc21ec
YanaKris <yana.krischenko@gmail.com>
2026-07-21 13:46:39 +0700

    getting users

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useEffect } from 'react';
+import './App.css';
+
 function App() {
-  return <h1>Таблица пользователей</h1>;
+  useEffect(() => {
+    fetch('https://dummyjson.com/users')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        console.log('Список пользователей:', data.users);
+        console.log(data.users.length, 'из', data.total);
+      });
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Пользователи</h1>
+      <p>Получили данные, вывод в косоль</p>
+    </div>
+  );
 }
 
 export default App;
